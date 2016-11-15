95ff821361b68b1a331df0c69a066599d695645c
Hector Climente <hclimente@gmail.com>
2016-11-15 17:52:54 +0100

    finished star_boxplot

--- R/star_boxplot.R
+++ R/star_boxplot.R
@@ -6,39 +6,33 @@
 #' 
 #' @param gg ggplot with a geom_boxplot to add significance marks.
 #' @param significance logical vector with the significant pairs.
+#' @param rm.outliers logical value the arc should fit the data with or without the outliers.
+#' @param fit logical value indicating if the arc should fit the shape of the data.
+#' @param dist numerical value indicating the distance from the arc to the data.
 #' @return gg ggplot with the geom_boxplot and the significance marks.
-star_boxplot <- function(gg,significance){
+star_boxplot <- function(gg,significance,rm.outliers=TRUE, fit = FALSE, dist = 0.05){
+  
+  pg <- ggplot_build(gg)
+  
+  data <- pg$data[[1]]
+  data$x.arc <- (data$xmax + data$xmin)/2
+  data$y.arc <- data$ymax
   
   if (rm.outliers){
-    plotRanges <- ddply(ranges, .(Cancer,Categories), summarise, Q1=quantile(y, 1/4), Q3=quantile(y, 3/4), IQR=Q3-Q1, upper.limit=Q3+1.5*IQR)
-    plotRanges <- ddply(plotRanges, .(Cancer), summarise, y=max(upper.limit))
+    data <- data[rep(significance, each = 2),]
   } else {
-    plotRanges <- ddply(ranges, .(Cancer), summarise, y=max(y))
+    plotRanges <- ddply(ranges, .(group), summarise, y=max(y))
   }
   
-  minStep <- max(plotRanges$y)/40
+  min_step <- max(data$y.arc)/60
+  model_arc <- create_model_arc(min_step, 0.25)
   
-  r <- 0.25
-  t <- seq(0, 180, by = 1) * pi / 180
-  x <- r * cos(t)
-  y <- minStep * sin(t)
-  y[which(y>minStep/2)] <- minStep/2
-  modelArc <- data.frame(x = x, y = y)
+  plot_symbols <- get_arcs_and_symbols(data, model_arc, min_step, fit, dist)
   
-  arcs <- data.frame(Cancer=character(),x_arc=numeric() ,y_arc=numeric())
-  ast <- data.frame(Cancer=character(),x_ast=numeric() ,y_ast=numeric())
-  for (kns in stat.tests$Cancer[stat.tests$p < 0.05]){
-    i <- which(categories==kns)
-    j <- max(plotRanges$y[plotRanges$Cancer==kns])
-    tmp_arc <- data.frame(Cancer=i, x_arc=modelArc$x+(i), y_arc=modelArc$y+j)
-    arcs <- rbind(arcs,tmp_arc)
-    
-    tmp_ast <- data.frame(Cancer=i,x_ast=i ,y_ast=j + 1.2*minStep)
-    ast <- rbind(ast,tmp_ast)
-  }
+  gg <- gg + 
+    geom_text(data=plot_symbols$asterisks, aes(x=x_ast,y=y_ast,label='*'), size = 15, inherit.aes = F) + 
+    geom_line(data=plot_symbols$arcs, aes(x_arc,y_arc, group=group), inherit.aes = F)
   
-  significanceElements <- list("asterisks" = ast, "arcs" = arcs)
-  
-  return(significanceElements)
+  return(gg)
   
 }
